9f6b9520038a986e84fb7d8afc262f66dab26023
jcarrionramos <javiercarrionramos@gmail.com>
2022-08-08 23:06:11 -0400

    WIP

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-var userRoutes = require('./routes/user.js')
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var userRoutes = require("./routes/user.js");
 
 // a middleware, maybe a good way to check the JWT
-app.use('/', (req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("A request for things received at " + Date.now());
   next();
-})
+});
 
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
 app.listen(3300);
